f24a18dab8152b28a1d5d4adc21b66668c090496
Jaeeun Park <pjcson8@naver.com>
2025-03-21 11:39:13 +0900

    feat: User service - Swagger 추가

---
+++ user/src/main/java/com/sibijo/user/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,77 @@
+package com.sibijo.user.infrastructure.config;
+
+
+import com.sibijo.user.presentation.dto.user.SignInRequestDto;
+import com.sibijo.user.presentation.dto.user.SignInResponseDto;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(apiInfo())
+                .path("/api/user/signIn", pathItem());
+    }
+
+    private Info apiInfo() {
+        return new Info().title("User REST API")
+                .description("User 관련 API 문서입니다.")
+                .version("1.0");
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    private PathItem pathItem() {
+        return new PathItem()
+                .post(
+                        new Operation()
+                                .summary("로그인")
+                                .description("Spring Security 필터를 통해 로그인 수행")
+                                .tags(List.of("auth-controller"))
+                                .requestBody(new RequestBody()
+                                        .content(new Content().addMediaType("application/json",
+                                                new MediaType().schema(new Schema<SignInRequestDto>().example(new SignInRequestDto("testUser", "password123"))))))
+                                .responses(new ApiResponses()
+                                        .addApiResponse("200", new ApiResponse().description("로그인 성공")
+                                                .content(new Content().addMediaType("application/json",
+                                                        new MediaType().schema(new Schema<com.sibijo.common.dto.ApiResponse<SignInResponseDto>>()
+                                                                .example(new com.sibijo.common.dto.ApiResponse<>(
+                                                                        "200",
+                                                                        "요청이 성공적으로 처리되었습니다.",
+                                                                        SignInResponseDto.builder().token("Bearer eygb...").build()
+                                                                ))))))
+                                        .addApiResponse("401", new ApiResponse().description("로그인 실패")
+                                                .content(new Content().addMediaType("application/json",
+                                                        new MediaType().schema(new Schema<com.sibijo.common.dto.ApiResponse<SignInResponseDto>>()
+                                                                .example(new com.sibijo.common.dto.ApiResponse<>(
+                                                                        "401",
+                                                                        "로그인 실패",
+                                                                        null
+                                                                ))))))
+                                )
+                );
+    }
+
+}
