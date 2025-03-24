ce52a89a0b336221456a9ff17c4a2bb962dfccd9
dohdong <goodstar7381@gmail.com>
2025-03-24 22:45:14 +0900

    fix: order쪽 실수로 바꾼 내용 수정

--- order/src/main/java/com/sibijo/order/infrastructure/client/Delivery/DeliveryClient.java
+++ order/src/main/java/com/sibijo/order/infrastructure/client/Delivery/DeliveryClient.java
@@ -1,8 +1,6 @@
 package com.sibijo.order.infrastructure.client.Delivery;
 
-import com.sibijo.common.dto.ApiResponse;
 import io.github.resilience4j.retry.annotation.Retry;
-import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +10,5 @@ public interface DeliveryClient {
 
     @Retry(name = "deliveryServiceRetry")
     @PostMapping("/api/deliveries")
-    ApiResponse<UUID> createDelivery(@RequestBody DeliveryRequestDto requestDto);
-
+    void createDelivery(@RequestBody DeliveryRequestDto requestDto);
 }
